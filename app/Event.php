26955427970ac10b63784cf8973c31a388ae4933
Rujal Shrestha <rujal4shrz@gmail.com>
2019-02-04 19:26:31 +0545

    Add Event.php model file

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $fillable = [
+    	'id',
+    	'event_name',
+    	'event_address',
+    	'event_venue-name',
+    	'event_category',
+    	'event_detail',
+    	'event_started_at',
+    	'event_ends_at',
+    	'organizer_name',
+    	'organizer_email',
+    	'organizer_contact_number',
+    	'organizer_detail',
+    	'ticket_name',
+    	'ticket_name',
+    	'ticket_number',
+    	'ticket_max_number',
+    	'ticket_min_number',
+    	'ticket_category',
+    	'ticket_expiry_date',
+    	'ticket_cover',
+    	'is_active',
+    	'remarks',
+    	'user_id',
+    	'id'
+    ];
 }
